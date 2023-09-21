96739f1f601ff11920764fbd86cace311125c170
marimo-star <144980938+marimo-star@users.noreply.github.com>
2023-09-21 23:58:01 +0000

    あああ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,49 @@
 import streamlit as st
+import math
 
 # Streamlitアプリケーションのタイトルを設定
-st.title("衛星打ち上げシミュレーションアプリ")
+st.title("衛星の軌道計算アプリ")
 
-# ユーザーに衛星の情報を入力してもらう
-satellite_name = st.text_input("山野1号", "")
-launch_date = st.date_input("1月10日", min_value=None, max_value=None)
-launch_vehicle = st.selectbox("打ち上げロケット", ["ロケットA", "ロケットB", "ロケットC"])
-payload_mass = st.number_input("50 (kg)", min_value=0.0, step=1.0)
+# ユーザーに衛星の初期高度と燃料の残量を入力してもらう
+initial_altitude = st.number_input("初期高度 (km)", min_value=0.0)
+remaining_fuel = st.number_input("残りの燃料量 (kg)", min_value=0.0)
 
 # ユーザーが「計算」ボタンをクリックしたときの処理
 if st.button("計算"):
-    # ここで衛星打ち上げのシミュレーションを実行し、結果を計算
-    # 例えば、打ち上げコスト、軌道への到達、成功確率などの情報を計算
+    # 地球の半径 (km)
+    earth_radius = 6371.0
+
+    # 衛星の初期高度 (km) を地球の半径に加える
+    initial_altitude_km = earth_radius + initial_altitude
+
+    # 衛星の速度を計算する関数
+    def calculate_velocity(altitude_km):
+        # 地球上の引力定数 (m^3/s^2/kg)
+        gravitational_constant = 6.67430e-11
+
+        # 地球の質量 (kg)
+        earth_mass = 5.972e24
+
+        # 衛星の高度 (m)
+        altitude_m = altitude_km * 1000
+
+        # 衛星の速度を計算 (m/s)
+        velocity = math.sqrt(gravitational_constant * earth_mass / (earth_radius + altitude_m))
+
+        return velocity
+
+    # 衛星の速度を計算
+    initial_velocity = calculate_velocity(initial_altitude_km)
+
+    # 衛星の軌道周回時間を計算 (秒)
+    orbital_period = (2 * math.pi * (earth_radius + initial_altitude_km)) / initial_velocity
+
+    # 衛星が燃料切れまでの時間を計算 (秒)
+    burnout_time = remaining_fuel / (orbital_period / 2)
 
     # 結果を表示
     st.subheader("結果")
-    st.write(f"衛星名: {satellite_name}")
-    st.write(f"打ち上げ日: {launch_date}")
-    st.write(f"打ち上げロケット: {launch_vehicle}")
-    st.write(f"ペイロード質量 (kg): {payload_mass}")
-    # ここで計算した結果を表示
-
-# 注意: 衛星打ち上げのシミュレーション部分は、具体的なアプリケーションの要件に合わせてカスタマイズする必要があります。
+    st.write(f"初期高度: {initial_altitude} km")
+    st.write(f"残りの燃料量: {remaining_fuel} kg")
+    st.write(f"衛星の軌道周回時間: {orbital_period} 秒")
+    st.write(f"燃料切れまでの時間: {burnout_time} 秒")
