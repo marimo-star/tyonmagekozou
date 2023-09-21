4b2e460e05cbb6d97d2557a2e9fe63164f88e121
marimo-star <144980938+marimo-star@users.noreply.github.com>
2023-09-21 23:53:37 +0000

    あああああ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,25 @@
 import streamlit as st
-import random
-st.title ("おみくじアプリ")
-if st.button("おみくじを引く"):
-    results=["大吉","中吉","小吉","吉","凶","大凶"]
-    result=random.choice(results)
-    st.write(f"結果：{result}")
 
+# Streamlitアプリケーションのタイトルを設定
+st.title("衛星打ち上げシミュレーションアプリ")
+
+# ユーザーに衛星の情報を入力してもらう
+satellite_name = st.text_input("山野1号", "")
+launch_date = st.date_input("1月10日", min_value=None, max_value=None)
+launch_vehicle = st.selectbox("打ち上げロケット", ["ロケットA", "ロケットB", "ロケットC"])
+payload_mass = st.number_input("50 (kg)", min_value=0.0, step=1.0)
+
+# ユーザーが「計算」ボタンをクリックしたときの処理
+if st.button("計算"):
+    # ここで衛星打ち上げのシミュレーションを実行し、結果を計算
+    # 例えば、打ち上げコスト、軌道への到達、成功確率などの情報を計算
+
+    # 結果を表示
+    st.subheader("結果")
+    st.write(f"衛星名: {satellite_name}")
+    st.write(f"打ち上げ日: {launch_date}")
+    st.write(f"打ち上げロケット: {launch_vehicle}")
+    st.write(f"ペイロード質量 (kg): {payload_mass}")
+    # ここで計算した結果を表示
+
+# 注意: 衛星打ち上げのシミュレーション部分は、具体的なアプリケーションの要件に合わせてカスタマイズする必要があります。
