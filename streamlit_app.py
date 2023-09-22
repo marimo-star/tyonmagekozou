ba7027049acfbc6c8380a66efaa0395812807a12
marimo-star <144980938+marimo-star@users.noreply.github.com>
2023-09-22 00:05:39 +0000

    ああああ

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,48 +2,48 @@ import streamlit as st
 import math
 
 # Streamlitアプリケーションのタイトルを設定
-st.title("衛星の軌道計算アプリ")
+st.title("宇宙から大気圏への落下時間計算アプリ")
 
-# ユーザーに衛星の初期高度と燃料の残量を入力してもらう
+# ユーザーに物体の情報を入力してもらう
+mass = st.number_input("物体の質量 (kg)", min_value=0.0)
+cross_sectional_area = st.number_input("物体の断面積 (m^2)", min_value=0.0)
 initial_altitude = st.number_input("初期高度 (km)", min_value=0.0)
-remaining_fuel = st.number_input("残りの燃料量 (kg)", min_value=0.0)
+initial_velocity = st.number_input("初速度 (m/s)", min_value=0.0)
+drag_coefficient = st.number_input("抗力係数 (0から2の間の値)", min_value=0.0, max_value=2.0)
 
 # ユーザーが「計算」ボタンをクリックしたときの処理
 if st.button("計算"):
-    # 地球の半径 (km)
-    earth_radius = 6371.0
+    # 定数
+    gravitational_acceleration = 9.81  # 重力加速度 (m/s^2)
+    earth_radius = 6371.0  # 地球の半径 (km)
 
-    # 衛星の初期高度 (km) を地球の半径に加える
-    initial_altitude_km = earth_radius + initial_altitude
+    # 初期高度 (m) を設定
+    initial_altitude_m = initial_altitude * 1000.0
 
-    # 衛星の速度を計算する関数
-    def calculate_velocity(altitude_km):
-        # 地球上の引力定数 (m^3/s^2/kg)
-        gravitational_constant = 6.67430e-11
+    # 大気密度 (kg/m^3) のモデル化
+    air_density = 1.225 * math.exp(-initial_altitude_m / 8500.0)
 
-        # 地球の質量 (kg)
-        earth_mass = 5.972e24
+    # 落下時間を計算
+    time_to_fall = 0.0
+    while initial_altitude_m > 0:
+        # 物体にかかる重力と抗力の計算
+        gravitational_force = mass * gravitational_acceleration
+        drag_force = 0.5 * air_density * initial_velocity**2 * cross_sectional_area * drag_coefficient
 
-        # 衛星の高度 (m)
-        altitude_m = altitude_km * 1000
+        # 速度の変化を計算
+        acceleration = (gravitational_force - drag_force) / mass
+        initial_velocity += acceleration
+        initial_altitude_m -= initial_velocity
 
-        # 衛星の速度を計算 (m/s)
-        velocity = math.sqrt(gravitational_constant * earth_mass / (earth_radius + altitude_m))
-
-        return velocity
-
-    # 衛星の速度を計算
-    initial_velocity = calculate_velocity(initial_altitude_km)
-
-    # 衛星の軌道周回時間を計算 (秒)
-    orbital_period = (2 * math.pi * (earth_radius + initial_altitude_km)) / initial_velocity
-
-    # 衛星が燃料切れまでの時間を計算 (秒)
-    burnout_time = remaining_fuel / (orbital_period / 2)
+        # 時間を更新
+        time_to_fall += 1  # 1秒ごとに更新
 
     # 結果を表示
     st.subheader("結果")
+    st.write(f"物体の質量: {mass} kg")
+    st.write(f"物体の断面積: {cross_sectional_area} m^2")
     st.write(f"初期高度: {initial_altitude} km")
-    st.write(f"残りの燃料量: {remaining_fuel} kg")
-    st.write(f"衛星の軌道周回時間: {orbital_period} 秒")
-    st.write(f"燃料切れまでの時間: {burnout_time} 秒")
+    st.write(f"初速度: {initial_velocity} m/s")
+    st.write(f"抗力係数: {drag_coefficient}")
+    st.write(f"落下時間: {time_to_fall} 秒")
+
